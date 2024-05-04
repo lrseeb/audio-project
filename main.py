386250e167a5c0f37e56546de63d3a639c5e8b53
Lauris Seebeck <lrseeb@utu.fi>
2024-05-04 21:47:02 +0300

    main muutoksia

--- main.py
+++ main.py
@@ -1,13 +1,23 @@
 import os
-import tensorflow as tf
-import numpy as np
-import seaborn as sns
-import pathlib
-from IPython import display
 from matplotlib import pyplot as plt
-from sklearn.metrics import classification_report
+import tensorflow as tf 
+import tensorflow_io as tfio
 
+LICK_FILE = os.path.join('samplet', 'lick_samplet', '1_lick.wav')
+NOT_LICK_FILE = os.path.join('samplet', 'noLick_samplet', '1_noLick.wav')
 
-training_set, validation_set = tf.keras.utils.audio_dataset_from_directory(
-    directory = './samplet/lick_samplet'
-)
+def load_wav_48k_mono(filename):
+    file_contents = tf.io.read_file(filename)
+    wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels = 1)
+    wav = tf.squeeze(wav, axis = -1)
+    sample_rate = tf.cast(sample_rate, dtype = tf.int64)
+    wav = tfio.audio.resample(wav, rate_in = sample_rate, rate_ou = 48000)
+    return wav
+
+wave = load_wav_48k_mono(LICK_FILE)
+nwave = load_wav_48k_mono(NOT_LICK_FILE)
+
+plt.plot(wave)
+plt.plot(nwave)
+plt.show()
+

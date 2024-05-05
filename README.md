79131b639da368e3ffcf9a27ecf19067bbbbc08e
Lauris Seebeck <lrseeb@utu.fi>
2024-05-05 18:05:52 +0300

    lick credits added, more sampling needed, and AI model started

--- README.md
+++ README.md
@@ -7,3 +7,15 @@ Real-time voice recognition code from AssemblyAI in youtube:
 https://www.youtube.com/watch?v=m-JzldXm9bQ&ab_channel=AssemblyAI
 
 The lick samples are gathered from youtube:
+https://youtu.be/hlY5fh3auR4?si=8ixIw3cZVaRdRgZR
+https://youtu.be/V4LQZhsoXAo?si=6F9mF51a7XVoOBea
+https://youtu.be/AY8oqFbGQPI?si=E7n_W77xiM-bwzsT
+https://youtu.be/1Cat9oXtwac?si=r-ssE7J90eTDZewX
+https://youtu.be/SVzK1znrBD0?si=Ah8k7r3gjA9MW-pk
+https://youtu.be/i_7bLbrbz-k?si=AJPNvnPQgqyGXacF
+https://youtu.be/F-MJCtpTBTU?si=jx_j1emSv8kDBnYT
+https://youtu.be/vh5bheh-Gqg?si=qiLxrcPCdJ4c-L_e
+https://youtu.be/RHOrq1bkScA?si=J7auPE6cEHBYXu0N
+https://youtu.be/inJz2DlFjM0?si=r_e6qD5TYKW6c2DE
+https://youtu.be/nKL2It6XzHA?si=2_c7T1L4dPxHVKZz
+https://youtu.be/krDxhnaKD7Q?si=4w2YL3-WRMcT0aBY
--- main.py
+++ main.py
@@ -3,24 +3,27 @@ from random import shuffle
 import tensorflow as tf 
 import tensorflow_io as tfio
 from matplotlib import pyplot as plt
+from keras.models import Sequential
+from keras.layers import Conv2D, Dense, Flatten
 
 LICK_FILE = os.path.join('samplet', 'lick_samplet', '1_lick.wav')
 NOT_LICK_FILE = os.path.join('samplet', 'noLick_samplet', '1_noLick.wav')
 
-def load_wav_48k_mono(filename):
+def load_wav_16k_mono(filename):
     file_contents = tf.io.read_file(filename)
     wav, sample_rate = tf.audio.decode_wav(file_contents, desired_channels = 1)
     wav = tf.squeeze(wav, axis = -1)
     sample_rate = tf.cast(sample_rate, dtype = tf.int64)
-    wav = tfio.audio.resample(wav, rate_in = sample_rate, rate_out = 48000)
+    wav = tfio.audio.resample(wav, rate_in = sample_rate, rate_out = 16000)
     return wav
 
-wave = load_wav_48k_mono(LICK_FILE)
-nwave = load_wav_48k_mono(NOT_LICK_FILE)
+wave = load_wav_16k_mono(LICK_FILE)
+nwave = load_wav_16k_mono(NOT_LICK_FILE)
 
+"""
 plt.plot(wave)
 plt.plot(nwave)
-plt.show()
+plt.show() """
 
 POS = os.path.join('samplet', 'lick_samplet')
 NEG = os.path.join('samplet', 'noLick_samplet')
@@ -34,7 +37,7 @@ data = positives.concatenate(negatives)
 
 lengths = []
 for file in os.listdir(os.path.join('samplet', 'lick_samplet')):
-    tensor_wave = load_wav_48k_mono(os.path.join('samplet', 'lick_samplet', file))
+    tensor_wave = load_wav_16k_mono(os.path.join('samplet', 'lick_samplet', file))
     lengths.append(len(tensor_wave))
 
 tf.math.reduce_mean(lengths)
@@ -42,9 +45,9 @@ tf.math.reduce_min(lengths)
 tf.math.reduce_max(lengths)
 
 def preprocess(file_path, label):
-    wav = load_wav_48k_mono(file_path)
-    wav = wav[:48000]
-    zero_padding = tf.zeros([48000] - tf.shape(wav), dtype = tf.float32)
+    wav = load_wav_16k_mono(file_path)
+    wav = wav[:80000]
+    zero_padding = tf.zeros([80000] - tf.shape(wav), dtype = tf.float32)
     wav = tf.concat([zero_padding, wav], 0)
     spectrogram = tf.signal.stft(wav, frame_length = 320, frame_step = 32)
     spectrogram = tf.abs(spectrogram)
@@ -55,6 +58,52 @@ filepath, label = positives.shuffle(buffer_size = 10000).as_numpy_iterator().nex
 
 spectrogram, label = preprocess(filepath, label)
 
+"""
 plt.figure(figsize = (30, 20))
 plt.imshow(tf.transpose(spectrogram)[0])
+plt.show()"""
+
+data = data.map(preprocess)
+data = data.cache()
+data = data.shuffle(buffer_size = 1000)
+data = data.batch(16)
+data = data.prefetch(8)
+
+print(len(data))
+
+train = data.take(9)
+test = data.skip(9).take(2)
+
+"""
+samples, labels = train.as_numpy_iterator().next()
+
+samples.shape
+
+model = Sequential()
+model.add(Conv2D(16, (3, 3), activation = 'relu', input_shape = (1491, 257, 1)))
+model.add(Conv2D(16, (3, 3), activation = 'relu'))
+model.add(Flatten())
+model.add(Dense(128, activation = 'relu'))
+model.add(Dense(1, activation = 'sigmoid'))
+
+model.compile('Adam', loss = 'BinaryCrossentropy', metrics = [tf.keras.metrics.Recall(), tf.keras.metrics.Precision()])
+
+model.summary()
+
+hist = model.fit(train, epochs = 4, validation_data = test)
+
+plt.title('Loss')
+plt.plot(hist.history['loss'], 'r')
+plt.plot(hist.history['val_loss'], 'b')
 plt.show()
+
+plt.title('Precision')
+plt.plot(hist.history['precision'], 'r')
+plt.plot(hist.history['val_precision'], 'b')
+plt.show()
+
+plt.title('Recall')
+plt.plot(hist.history['recall'], 'r')
+plt.plot(hist.history['val_recall'], 'b')
+plt.show()
+"""
